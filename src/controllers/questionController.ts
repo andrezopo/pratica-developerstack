7428deb4f6093953f3bc641ff65ec65bc10edd4a
André Zopolato <andrezopo@gmail.com>
2022-09-08 20:43:44 -0300

    fix: corrige o objeto de retorno de get all

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -22,7 +22,7 @@ export async function createAnswer(req: Request, res: Response) {
 export async function get(req: Request, res: Response) {
   // TODO
   const questions = await questionService.get();
-  res.status(200).send(questions);
+  res.status(200).send({ questions });
 }
 
 export async function getById(req: Request, res: Response) {
